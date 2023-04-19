46e380c43a68b348c687582ea7147fc7e7bd0098
Dilpreet360Circle <dilpreet@360cicleinfotech.com>
2023-04-19 12:55:12 +0530

    [new]

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'; 
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+app.get('/about',(req, res)=>{
+    res.render('about');
+});
+app.get('/contact',(req, res)=>{
+    res.render('contact');
+});
+app.get('*',(req, res)=>{
+    res.render('error');
+});
+app.listen(3000);
